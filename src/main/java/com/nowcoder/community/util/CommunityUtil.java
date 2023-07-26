14984ddc186882a0f42f6b7a8135d315e45fbc5e
Wangdan-code <15539354565@163.com>
2023-07-26 10:44:39 +0800

    登录页面的验证码功能：Kaptcha

--- src/main/java/com/nowcoder/community/util/CommunityUtil.java
+++ src/main/java/com/nowcoder/community/util/CommunityUtil.java
@@ -6,6 +6,8 @@ import org.springframework.util.DigestUtils;
 import java.util.UUID;
 
 public class CommunityUtil {
+    public static String generateUUID;
+
     //生成随机字符串
     public static String generateUUID() {
         return UUID.randomUUID().toString().replaceAll("-", "");
