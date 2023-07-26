127c5d7c8eb2d94a1e000e6045b4a6f45c79d7ec
Wangdan-code <15539354565@163.com>
2023-07-26 16:58:48 +0800

    开发登录、退出功能+拦截器显示登录消息

---
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.controller.Interceptor.AlphaInterceptor;
+import com.nowcoder.community.controller.Interceptor.LoginTicketInteceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private AlphaInterceptor alphaInterceptor;
+
+    @Autowired
+    private LoginTicketInteceptor loginTicketInteceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(alphaInterceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg")
+                .addPathPatterns("/register","/login");//排除哪些路径不需要拦截 增加哪些路径拦截
+        registry.addInterceptor(loginTicketInteceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");//排除哪些路径不需要拦截 增加哪些路径拦截
+    }
+}
