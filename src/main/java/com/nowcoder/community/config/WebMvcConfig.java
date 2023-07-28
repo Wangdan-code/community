793ad9213d3bc8eb0d8c79871bc02396587ebef4
Wangdan-code <15539354565@163.com>
2023-07-28 10:53:39 +0800

    自定义注解，拦截器，检查登录状态

--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.config;
 
 import com.nowcoder.community.controller.Interceptor.AlphaInterceptor;
+import com.nowcoder.community.controller.Interceptor.LoginRequiredInterceptor;
 import com.nowcoder.community.controller.Interceptor.LoginTicketInteceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInteceptor loginTicketInteceptor;
 
+    @Autowired
+    private LoginRequiredInterceptor loginRequiredInterceptor;
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
@@ -22,5 +25,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addPathPatterns("/register","/login");//排除哪些路径不需要拦截 增加哪些路径拦截
         registry.addInterceptor(loginTicketInteceptor)
                 .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");//排除哪些路径不需要拦截 增加哪些路径拦截
+        registry.addInterceptor(loginRequiredInterceptor)
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.png","/**/*.jpg","/**/*.jpeg");
     }
 }
--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
@@ -43,12 +44,13 @@ public class UserController {
 
     @Autowired
     private HostHolder hostHolder;
-
+    @LoginRequired
     @RequestMapping(path = "/setting",method = RequestMethod.GET)
     public String getSettingPage(){
         return "/site/setting";
     }
 
+    @LoginRequired
     @RequestMapping(path = "/upload",method = RequestMethod.POST)
     public String uploadHeader(MultipartFile headerImage, Model model){
         if(headerImage == null){
@@ -101,9 +103,9 @@ public class UserController {
             logger.error("读取头像失败: " + e.getMessage());
         }
     }
-
+    @LoginRequired
     @RequestMapping(path = "/uploadPassword",method = RequestMethod.POST)
-    public String uploadHeader(String origin_password,String new_password,String new_password2, Model model){
+    public String uploadPassword(String origin_password,String new_password,String new_password2, Model model){
         User user = hostHolder.getUser();
         int userId = user.getId();
         Map<String,Object> map = userService.updatePassword(origin_password,new_password,new_password2,userId);
