e470b37836eea569ca2a6d9da00b7dd91c19562c
Wangdan-code <15539354565@163.com>
2023-08-23 15:09:51 +0800

    发布帖子功能完成

--- src/main/java/com/nowcoder/community/Dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/Dao/DiscussPostMapper.java
@@ -12,4 +12,6 @@ public interface DiscussPostMapper {
     //@Param注解用于参数取别名，如果只有一个参数，并且在<if>里使用，就必须加别名
     int selectDiscussPostRows(@Param("userId") int userId); //查询表的行数 注解起别名字
 
+    int insertDiscussPost(DiscussPost discussPost);
+
 }
--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -2,8 +2,10 @@ package com.nowcoder.community.service;
 
 import com.nowcoder.community.Dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -11,10 +13,29 @@ import java.util.List;
 public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
     public List<DiscussPost> findDisscussPosts(int userId, int offset,int limit){
         return discussPostMapper.selectDiscussPosts(userId,offset,limit);
     }
     public int findDisscussRows(int userId){
         return discussPostMapper.selectDiscussPostRows(userId);
+    }
+
+    public int addDiscussPost(DiscussPost post){
+        if(post == null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        //post中的title和content需要过滤敏感词，最好也处理标签
+        //转义html标记录
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+        //过滤敏感词
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
     }
 }
